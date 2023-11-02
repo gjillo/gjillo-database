c2356b1d0377be6a2d966ab25b945a47269c2762
Krzysztof Wrona <rubikon02@gmail.com>
2023-11-02 20:21:19 +0100

    Add card field values getter function

---
+++ card_fields.sql
@@ -0,0 +1,87 @@
+SELECT
+    date_values.card_id,
+    date_values.field_id,
+    date_values.value AS date_value,
+    checkbox_values.value AS checkbox_value,
+    number_values.value AS number_value,
+    select_options.value AS select_value,
+    select_options.color AS select_color,
+    text_values.value AS text_value
+FROM date_values
+LEFT JOIN checkbox_values ON date_values.field_id = checkbox_values.field_id
+LEFT JOIN number_values ON date_values.field_id = number_values.field_id
+LEFT JOIN select_values ON date_values.field_id = select_values.field_id
+LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
+LEFT JOIN text_values ON date_values.field_id = text_values.field_id
+
+UNION
+
+SELECT
+    checkbox_values.card_id,
+    checkbox_values.field_id,
+    date_values.value AS date_value,
+    checkbox_values.value AS checkbox_value,
+    number_values.value AS number_value,
+    select_options.value AS select_value,
+    select_options.color AS select_color,
+    text_values.value AS text_value
+FROM checkbox_values
+LEFT JOIN date_values ON checkbox_values.field_id = date_values.field_id
+LEFT JOIN number_values ON checkbox_values.field_id = number_values.field_id
+LEFT JOIN select_values ON checkbox_values.field_id = select_values.field_id
+LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
+LEFT JOIN text_values ON checkbox_values.field_id = text_values.field_id
+
+UNION
+
+SELECT
+    number_values.card_id,
+    number_values.field_id,
+    date_values.value AS date_value,
+    checkbox_values.value AS checkbox_value,
+    number_values.value AS number_value,
+    select_options.value AS select_value,
+    select_options.color AS select_color,
+    text_values.value AS text_value
+FROM number_values
+LEFT JOIN date_values ON number_values.field_id = date_values.field_id
+LEFT JOIN checkbox_values ON number_values.field_id = checkbox_values.field_id
+LEFT JOIN select_values ON number_values.field_id = select_values.field_id
+LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
+LEFT JOIN text_values ON number_values.field_id = text_values.field_id
+
+UNION
+
+SELECT
+    select_values.card_id,
+    select_values.field_id,
+    date_values.value AS date_value,
+    checkbox_values.value AS checkbox_value,
+    number_values.value AS number_value,
+    select_options.value AS select_value,
+    select_options.color AS select_color,
+    text_values.value AS text_value
+FROM select_values
+LEFT JOIN date_values ON select_values.field_id = date_values.field_id
+LEFT JOIN checkbox_values ON select_values.field_id = checkbox_values.field_id
+LEFT JOIN number_values ON select_values.field_id = number_values.field_id
+LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
+LEFT JOIN text_values ON select_values.field_id = text_values.field_id
+
+UNION
+
+SELECT
+    text_values.card_id,
+    text_values.field_id,
+    date_values.value AS date_value,
+    checkbox_values.value AS checkbox_value,
+    number_values.value AS number_value,
+    select_options.value AS select_value,
+    select_options.color AS select_color,
+    text_values.value AS text_value
+FROM text_values
+LEFT JOIN date_values ON text_values.field_id = date_values.field_id
+LEFT JOIN checkbox_values ON text_values.field_id = checkbox_values.field_id
+LEFT JOIN number_values ON text_values.field_id = number_values.field_id
+LEFT JOIN select_values ON text_values.field_id = select_values.field_id
+LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id;
