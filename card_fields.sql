69f1fdc7b28f00be17603b9844fb58b9f666063f
Krzysztof Wrona <rubikon02@gmail.com>
2023-11-11 13:18:04 +0100

    Change id to uuid

--- card_fields.sql
+++ card_fields.sql
@@ -1,6 +1,6 @@
 SELECT
-    date_values.card_id,
-    date_values.field_id,
+    date_values.card_uuid,
+    date_values.field_uuid,
     date_values.value AS date_value,
     checkbox_values.value AS checkbox_value,
     number_values.value AS number_value,
@@ -8,17 +8,17 @@ SELECT
     select_options.color AS select_color,
     text_values.value AS text_value
 FROM date_values
-LEFT JOIN checkbox_values ON date_values.field_id = checkbox_values.field_id
-LEFT JOIN number_values ON date_values.field_id = number_values.field_id
-LEFT JOIN select_values ON date_values.field_id = select_values.field_id
-LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
-LEFT JOIN text_values ON date_values.field_id = text_values.field_id
+LEFT JOIN checkbox_values ON date_values.field_uuid = checkbox_values.field_uuid
+LEFT JOIN number_values ON date_values.field_uuid = number_values.field_uuid
+LEFT JOIN select_values ON date_values.field_uuid = select_values.field_uuid
+LEFT JOIN select_options ON select_values.select_option_uuid = select_options.select_option_uuid
+LEFT JOIN text_values ON date_values.field_uuid = text_values.field_uuid
 
 UNION
 
 SELECT
-    checkbox_values.card_id,
-    checkbox_values.field_id,
+    checkbox_values.card_uuid,
+    checkbox_values.field_uuid,
     date_values.value AS date_value,
     checkbox_values.value AS checkbox_value,
     number_values.value AS number_value,
@@ -26,17 +26,17 @@ SELECT
     select_options.color AS select_color,
     text_values.value AS text_value
 FROM checkbox_values
-LEFT JOIN date_values ON checkbox_values.field_id = date_values.field_id
-LEFT JOIN number_values ON checkbox_values.field_id = number_values.field_id
-LEFT JOIN select_values ON checkbox_values.field_id = select_values.field_id
-LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
-LEFT JOIN text_values ON checkbox_values.field_id = text_values.field_id
+LEFT JOIN date_values ON checkbox_values.field_uuid = date_values.field_uuid
+LEFT JOIN number_values ON checkbox_values.field_uuid = number_values.field_uuid
+LEFT JOIN select_values ON checkbox_values.field_uuid = select_values.field_uuid
+LEFT JOIN select_options ON select_values.select_option_uuid = select_options.select_option_uuid
+LEFT JOIN text_values ON checkbox_values.field_uuid = text_values.field_uuid
 
 UNION
 
 SELECT
-    number_values.card_id,
-    number_values.field_id,
+    number_values.card_uuid,
+    number_values.field_uuid,
     date_values.value AS date_value,
     checkbox_values.value AS checkbox_value,
     number_values.value AS number_value,
@@ -44,17 +44,17 @@ SELECT
     select_options.color AS select_color,
     text_values.value AS text_value
 FROM number_values
-LEFT JOIN date_values ON number_values.field_id = date_values.field_id
-LEFT JOIN checkbox_values ON number_values.field_id = checkbox_values.field_id
-LEFT JOIN select_values ON number_values.field_id = select_values.field_id
-LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
-LEFT JOIN text_values ON number_values.field_id = text_values.field_id
+LEFT JOIN date_values ON number_values.field_uuid = date_values.field_uuid
+LEFT JOIN checkbox_values ON number_values.field_uuid = checkbox_values.field_uuid
+LEFT JOIN select_values ON number_values.field_uuid = select_values.field_uuid
+LEFT JOIN select_options ON select_values.select_option_uuid = select_options.select_option_uuid
+LEFT JOIN text_values ON number_values.field_uuid = text_values.field_uuid
 
 UNION
 
 SELECT
-    select_values.card_id,
-    select_values.field_id,
+    select_values.card_uuid,
+    select_values.field_uuid,
     date_values.value AS date_value,
     checkbox_values.value AS checkbox_value,
     number_values.value AS number_value,
@@ -62,17 +62,17 @@ SELECT
     select_options.color AS select_color,
     text_values.value AS text_value
 FROM select_values
-LEFT JOIN date_values ON select_values.field_id = date_values.field_id
-LEFT JOIN checkbox_values ON select_values.field_id = checkbox_values.field_id
-LEFT JOIN number_values ON select_values.field_id = number_values.field_id
-LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id
-LEFT JOIN text_values ON select_values.field_id = text_values.field_id
+LEFT JOIN date_values ON select_values.field_uuid = date_values.field_uuid
+LEFT JOIN checkbox_values ON select_values.field_uuid = checkbox_values.field_uuid
+LEFT JOIN number_values ON select_values.field_uuid = number_values.field_uuid
+LEFT JOIN select_options ON select_values.select_option_uuid = select_options.select_option_uuid
+LEFT JOIN text_values ON select_values.field_uuid = text_values.field_uuid
 
 UNION
 
 SELECT
-    text_values.card_id,
-    text_values.field_id,
+    text_values.card_uuid,
+    text_values.field_uuid,
     date_values.value AS date_value,
     checkbox_values.value AS checkbox_value,
     number_values.value AS number_value,
@@ -80,8 +80,8 @@ SELECT
     select_options.color AS select_color,
     text_values.value AS text_value
 FROM text_values
-LEFT JOIN date_values ON text_values.field_id = date_values.field_id
-LEFT JOIN checkbox_values ON text_values.field_id = checkbox_values.field_id
-LEFT JOIN number_values ON text_values.field_id = number_values.field_id
-LEFT JOIN select_values ON text_values.field_id = select_values.field_id
-LEFT JOIN select_options ON select_values.select_option_id = select_options.select_option_id;
+LEFT JOIN date_values ON text_values.field_uuid = date_values.field_uuid
+LEFT JOIN checkbox_values ON text_values.field_uuid = checkbox_values.field_uuid
+LEFT JOIN number_values ON text_values.field_uuid = number_values.field_uuid
+LEFT JOIN select_values ON text_values.field_uuid = select_values.field_uuid
+LEFT JOIN select_options ON select_values.select_option_uuid = select_options.select_option_uuid;
